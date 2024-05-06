9b3884fde65456187b5cc6c62c459f637365d166
James Prior <jamesgr.prior@gmail.com>
2024-05-06 07:43:04 +0100

    Add more test cases

--- python/tests/test_non_standard.py
+++ python/tests/test_non_standard.py
@@ -131,4 +131,126 @@ def test_location_of_keys_from_array() -> None:
     assert find(nodes[0][1], data) == [("a", "$['some'][~'a']")]
 
 
-# TODO: Port docs example test cases
+@dataclasses.dataclass
+class DocsTestCase:
+    description: str
+    query: str
+    data: JSONValue
+    want: list[JSONValue]
+    want_paths: list[str]
+
+
+DOCS_TEST_CASES: list[DocsTestCase] = [
+    DocsTestCase(
+        description="key selector, key of nested object",
+        query="$.a[0].~c",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=["c"],
+        want_paths=["$['a'][0][~'c']"],
+    ),
+    DocsTestCase(
+        description="key selector, key does not exist",
+        query="$.a[1].~c",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=[],
+        want_paths=[],
+    ),
+    DocsTestCase(
+        description="key selector, descendant, single quoted key",
+        query="$..[~'b']",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=["b", "b"],
+        want_paths=["$['a'][0][~'b']", "$['a'][1][~'b']"],
+    ),
+    DocsTestCase(
+        description="key selector, , descendant, double quoted key",
+        query='$..[~"b"]',
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=["b", "b"],
+        want_paths=["$['a'][0][~'b']", "$['a'][1][~'b']"],
+    ),
+    DocsTestCase(
+        description="keys selector, object key",
+        query="$.a[0].~",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=["b", "c"],
+        want_paths=["$['a'][0][~'b']", "$['a'][0][~'c']"],
+    ),
+    DocsTestCase(
+        description="keys selector, array key",
+        query="$.a.~",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=[],
+        want_paths=[],
+    ),
+    DocsTestCase(
+        description="keys selector, descendant keys",
+        query="$..[~]",
+        data={
+            "a": [{"b": "x", "c": "z"}, {"b": "y"}],
+        },
+        want=["a", "b", "c", "b"],
+        want_paths=[
+            "$[~'a']",
+            "$['a'][0][~'b']",
+            "$['a'][0][~'c']",
+            "$['a'][1][~'b']",
+        ],
+    ),
+    DocsTestCase(
+        description="keys filter selector, conditionally select object keys",
+        query="$.*[~?length(@) > 2]",
+        data=[{"a": [1, 2, 3], "b": [4, 5]}, {"c": {"x": [1, 2]}}, {"d": [1, 2, 3]}],
+        want=["a", "d"],
+        want_paths=["$[0][~'a']", "$[2][~'d']"],
+    ),
+    DocsTestCase(
+        description="keys filter selector, existence test",
+        query="$.*[~?@.x]",
+        data=[{"a": [1, 2, 3], "b": [4, 5]}, {"c": {"x": [1, 2]}}, {"d": [1, 2, 3]}],
+        want=["c"],
+        want_paths=["$[1][~'c']"],
+    ),
+    DocsTestCase(
+        description="keys filter selector, keys from an array",
+        query="$[~?(true == true)]",
+        data=[{"a": [1, 2, 3], "b": [4, 5]}, {"c": {"x": [1, 2]}}, {"d": [1, 2, 3]}],
+        want=[],
+        want_paths=[],
+    ),
+    DocsTestCase(
+        description="current key identifier, match on object names",
+        query="$[?match(#, '^ab.*') && length(@) > 0 ]",
+        data={"abc": [1, 2, 3], "def": [4, 5], "abx": [6], "aby": []},
+        want=[[1, 2, 3], [6]],
+        want_paths=["$['abc']", "$['abx']"],
+    ),
+    DocsTestCase(
+        description="current key identifier, compare current array index",
+        query="$.abc[?(# >= 1)]",
+        data={"abc": [1, 2, 3], "def": [4, 5], "abx": [6], "aby": []},
+        want=[2, 3],
+        want_paths=["$['abc'][1]", "$['abc'][2]"],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "case", DOCS_TEST_CASES, ids=operator.attrgetter("description")
+)
+def test_docs_examples(case: DocsTestCase) -> None:
+    nodes = find(case.query, case.data)
+    assert nodes.values() == case.want
+    assert nodes.paths() == case.want_paths
