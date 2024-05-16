f5b2e0cbb800c3f72bef64588539f5e61067e704
James Prior <jamesgr.prior@gmail.com>
2024-05-16 08:05:29 +0100

    Map I-Regexp to Python re and update CTS

--- python/jpq/function_extensions/match.py
+++ python/jpq/function_extensions/match.py
@@ -1,9 +1,12 @@
 """The standard `match` function extension."""
 
 import regex as re
+from iregexp_check import check
 
 from jpq import ExpressionType
 from jpq import FilterFunction
+
+from ._pattern import map_re
 
 
 class Match(FilterFunction):
@@ -12,10 +15,14 @@ class Match(FilterFunction):
     arg_types = [ExpressionType.Value, ExpressionType.Value]
     return_type = ExpressionType.Logical
 
-    def __call__(self, string: str, pattern: str) -> bool:
+    def __call__(self, string: str, pattern: object) -> bool:
         """Return `True` if _string_ matches _pattern_, or `False` otherwise."""
+        # TODO: our own cache that includes the i-regexp check and map
+        if not isinstance(pattern, str) or not check(pattern):
+            return False
+
         try:
             # re.fullmatch caches compiled patterns internally
-            return bool(re.fullmatch(pattern, string))
+            return bool(re.fullmatch(map_re(pattern), string))
         except (TypeError, re.error):
             return False
--- python/jpq/function_extensions/search.py
+++ python/jpq/function_extensions/search.py
@@ -1,9 +1,12 @@
 """The standard `search` function extension."""
 
 import regex as re
+from iregexp_check import check
 
 from jpq import ExpressionType
 from jpq import FilterFunction
+
+from ._pattern import map_re
 
 
 class Search(FilterFunction):
@@ -12,10 +15,14 @@ class Search(FilterFunction):
     arg_types = [ExpressionType.Value, ExpressionType.Value]
     return_type = ExpressionType.Logical
 
-    def __call__(self, string: str, pattern: str) -> bool:
+    def __call__(self, string: str, pattern: object) -> bool:
         """Return `True` if _string_ contains _pattern_, or `False` otherwise."""
+        # TODO: our own cache that includes the i-regexp check and map
+        if not isinstance(pattern, str) or not check(pattern):
+            return False
+
         try:
             # re.search caches compiled patterns internally
-            return bool(re.search(pattern, string))
+            return bool(re.search(map_re(pattern), string))
         except (TypeError, re.error):
             return False
